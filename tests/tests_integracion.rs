4f8b12a5a6c108e6762dcf748062a961ef08deb1
Facundo de la Plata <fdelaplata@fi.uba.ar>
2023-04-30 21:50:12 -0300

    Implemento tests de integración

--- tests/tests_integracion.rs
+++ tests/tests_integracion.rs
@@ -1,69 +1,70 @@
-//use tp_individual::{evaluar_jugada, obtener_piezas_archivo};
-// Falta implementar estos tests desde archivos
-/*
+use std::fs;
+
+use tp_individual::{evaluar_jugada, obtener_piezas_archivo};
+
 #[test]
-fn test_integracion_negras_capturan() {
-    let input = "_ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ D _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ t _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    ";
-    let piezas;
-    piezas = obtener_piezas_archivo(&String::from(input));
-    let resultado = evaluar_jugada(&piezas[0], &piezas[1]);
+fn test_integracion_negras_capturan() -> Result<(), &'static str> {
+    let file_path = "tests/data/table_test_1.txt".to_string();
+
+    let content_result = fs::read_to_string(file_path);
+    let content = match content_result {
+        Ok(file) => file,
+        Err(_e) => return Err("Error al leer el archivo"),
+    };
+
+    let piezas = obtener_piezas_archivo(&content)?;
+    let resultado = evaluar_jugada(&piezas[0], &piezas[1])?;
+
     assert_eq!(resultado, "N");
-}
-#[test]
-fn test_integracion_blancas_capturan() {
-    let input = "_ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ P _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ a _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    ";
-    let piezas;
-    piezas = obtener_piezas_archivo(&String::from(input));
-    let resultado = evaluar_jugada(&piezas[0], &piezas[1]);
-    assert_eq!(resultado, "B");
-}
-#[test]
-fn test_integracion_empate() {
-    let input = "_ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ R _ _ _ _ _
-    _ _ t _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    ";
-    let piezas;
-    piezas = obtener_piezas_archivo(&String::from(input));
-    let resultado = evaluar_jugada(&piezas[0], &piezas[1]);
-    assert_eq!(resultado, "E");
-}
-#[test]
-fn test_integracion_todos_pierden() {
-    let input = "_ _ _ _ _ _ _ _
-    _ _ _ _ _ P _ _
-    _ _ _ _ _ _ _ _
-    _ _ d _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    ";
-    let piezas;
-    piezas = obtener_piezas_archivo(&String::from(input));
-    let resultado = evaluar_jugada(&piezas[0], &piezas[1]);
-    assert_eq!(resultado, "P");
+    Ok(())
 }
 
-*/
+#[test]
+fn test_integracion_blancas_capturan() -> Result<(), &'static str> {
+    let file_path = "tests/data/table_test_2.txt".to_string();
+
+    let content_result = fs::read_to_string(file_path);
+    let content = match content_result {
+        Ok(file) => file,
+        Err(_e) => return Err("Error al leer el archivo"),
+    };
+
+    let piezas = obtener_piezas_archivo(&content)?;
+    let resultado = evaluar_jugada(&piezas[0], &piezas[1])?;
+
+    assert_eq!(resultado, "B");
+    Ok(())
+}
+#[test]
+fn test_integracion_empate() -> Result<(), &'static str> {
+    let file_path = "tests/data/table_test_3.txt".to_string();
+
+    let content_result = fs::read_to_string(file_path);
+    let content = match content_result {
+        Ok(file) => file,
+        Err(_e) => return Err("Error al leer el archivo"),
+    };
+
+    let piezas = obtener_piezas_archivo(&content)?;
+
+    let resultado = evaluar_jugada(&piezas[0], &piezas[1])?;
+
+    assert_eq!(resultado, "E");
+    Ok(())
+}
+
+#[test]
+fn test_integracion_todos_pierden() -> Result<(), &'static str> {
+    let file_path = "tests/data/table_test_4.txt".to_string();
+
+    let content_result = fs::read_to_string(file_path);
+    let content = match content_result {
+        Ok(file) => file,
+        Err(_e) => return Err("Error al leer el archivo"),
+    };
+    let piezas = obtener_piezas_archivo(&content)?;
+    let resultado = evaluar_jugada(&piezas[0], &piezas[1])?;
+
+    assert_eq!(resultado, "P");
+    Ok(())
+}
